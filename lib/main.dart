cc2c1f5689bf4bf28b8d447583f3bced81230a6b
Muhammad Umer <mhadisb1212@gmail.com>
2024-02-15 02:41:29 +0500

    Bottom Sheet Added

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: Home(),
     );
   }
 }
 
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            _displayBottomSheet(context);
+          },
+          height: 50,
+          minWidth: 200,
+          color: Colors.blue.shade700,
+          child: const Text(
+            'OPEN',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future _displayBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        backgroundColor: Colors.blue.shade400,
+        barrierColor: Colors.black38.withOpacity(0.4),
+        isDismissible: true,
+        builder: (context) => const SizedBox(
+              height: 200,
+              child: Center(
+                  child: Text(
+                "What You Want? \n Close me 😑",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25),
+              )),
+            ));
   }
 }
