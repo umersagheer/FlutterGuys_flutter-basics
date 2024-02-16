9d015b7b946d2ec5d82cc17bcd9943cd0f040d43
Muhammad Umer <mhadisb1212@gmail.com>
2024-02-16 19:49:06 +0500

    date-picker added

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController _dataController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Boilerplate")),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: TextField(
+            controller: _dataController,
+            decoration: const InputDecoration(
+                labelText: 'DATE',
+                filled: true,
+                prefixIcon: Icon(Icons.calendar_today),
+                enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                )),
+            readOnly: true,
+            onTap: () {
+              _selectdate();
+            },
+          ),
+        ),
+      ),
     );
+  }
+
+  Future<void> _selectdate() async {
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100));
+    if (picked != null) {
+      _dataController.text = picked.toString().split(" ")[0];
+    }
   }
 }
