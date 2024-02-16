d6a1a3757303d85179132f091dfdf9c31ab33393
Muhammad Umer <mhadisb1212@gmail.com>
2024-02-16 17:02:52 +0500

    dropdown-menu added

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,55 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _dropdownValue = '1';
+
+  final _items = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Boilerplate")),
+      body: Center(
+        child: Container(
+          width: 120,
+          height: 60,
+          decoration: BoxDecoration(
+              color: Colors.teal.shade300,
+              borderRadius: BorderRadius.circular(10)),
+          child: Center(
+            child: DropdownButton(
+              items: _items.map((String item) {
+                return DropdownMenuItem(value: item, child: Text(item));
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _dropdownValue = newValue!;
+                });
+              },
+              value: _dropdownValue,
+              dropdownColor: Colors.teal.shade200,
+              borderRadius: BorderRadius.circular(10),
+              icon: const Icon(Icons.keyboard_arrow_down),
+              iconSize: 30,
+              style: TextStyle(fontSize: 20, color: Colors.grey.shade800),
+              underline: const SizedBox(),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
